ca38b1981400e0fd3087dd6baeb94f316b36b157
Venatoral <15882190474@163.com>
2020-09-30 20:20:48 +0800

    update

--- src/credit.py
+++ src/credit.py
@@ -1,14 +1,22 @@
 # coding = utf-8
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
 
 # datasets
-record_file_name = '../data/application_record.csv'
-credit_file_name = '../data/credit_record.csv'
+appdata_file_name = '../data/application_record.csv'
+record_file_name = '../data/credit_record.csv'
+
+
+# read and clean up the datasets
+def load_datasets():
+    # application_record.csv contains appliers personal information.(feature)
+    app_data = pd.read_csv(appdata_file_name).drop_duplicates()
+    # credit_record.csv records users' behaviors of credit card.
+    record_data = pd.read_csv(record_file_name)
+    # merge datasets
+    credit_data = pd.merge(left=app_data, right=record_data, on='ID')
+
+
 if __name__ == '__main__':
-    # read datasets into DataFrame using pandas
-    credit_df = pd.read_csv(credit_file_name)
-    record_df = pd.read_csv(record_file_name)
-    # show info about dataFrame
-    print(credit_df.dropna().info())
-    print(record_df.dropna().info())
+    load_datasets()
