78993efcd3f5f04e730b4eb23f8c37643d38ddb7
Obscurepawn <2018091620007@std.uestc.edu.cn>
2020-10-20 15:20:30 +0800

    organize files

--- src/bpNeuralNetworks.py
+++ src/bpNeuralNetworks.py
@@ -4,75 +4,11 @@ from torch.nn import init
 import matplotlib.pyplot as plt
 from transCoding import *
 import numpy as np
+from confusionMatrix import *
 import itertools
 
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
-
-
-def plot_confusion_matrix(cm,
-                          target_names,
-                          title='Confusion matrix',
-                          cmap=None,
-                          normalize=True):
-    """
-    given a sklearn confusion matrix (cm), make a nice plot
-
-    Arguments
-    ---------
-    cm:           confusion matrix from sklearn.metrics.confusion_matrix
-    target_names: given classification classes such as [0, 1, 2]
-                  the class names, for example: ['high', 'medium', 'low']
-    title:        the text to display at the top of the matrix
-    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
-                  see:
-                  http://matplotlib.org/examples/color/colormaps_reference.html
-                  plt.get_cmap('jet') or plt.cm.Blues
-    normalize:    If False, plot the raw numbers
-                  If True, plot the proportions
-    Usage
-    -----
-    plot_confusion_matrix(cm = cm, 
-                           normalize = True, # show proportions
-                          target_names = y_labels_vals, # list of classes names
-                          title = best_estimator_name) # title of graph
-    """
-
-    accuracy = np.trace(cm) / np.sum(cm).astype('float')
-    misclass = 1 - accuracy
-
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
-    plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(
-        accuracy, misclass))
-    plt.show()
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 
 def weights_init(m):
@@ -264,7 +200,8 @@ def calculator(dropoutP1=0.06, dropoutP2=0.03,
         plt.text(2000, 0.1, 'TestZeroRecall'+str(testZeroRecallList[-1]))
 
         plt.show()
-        plot_confusion_matrix(np.array([[testOneRecallList[-1],1-testOneRecallList[-1]],[1-testZeroRecallList[-1],testZeroRecallList[-1]]]),[u'正常用户',u'失信用户'])
+        plot_confusion_matrix(np.array([[testOneRecallList[-1], 1-testOneRecallList[-1]], [
+                              1-testZeroRecallList[-1], testZeroRecallList[-1]]]), [u'正常用户', u'失信用户'])
         return 0
     else:
         net.eval()
